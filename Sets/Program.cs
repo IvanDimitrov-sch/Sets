8e183d8b63bc911579de1dd272ebb743bc027006
MEGA <MEGA@gmail.com>
2025-04-07 18:59:02 +0300

    ex 3 done finally ?

--- Sets/Program.cs
+++ Sets/Program.cs
@@ -39,7 +39,7 @@
         static void Zad3()
         {
             HashSet<string> list = new HashSet<string>();
-            list = Console.ReadLine().Split(' ').Select(x => x.ToLower()).ToHashSet();
+            list = Console.ReadLine().Split( ' ', ',', ';', '!', '.' ).Select(x => x.ToLower()).ToHashSet();
             Console.WriteLine($"The sentence has {list.Count} words and they are {String.Join(',', list)}");
         }
         static void Zad4()

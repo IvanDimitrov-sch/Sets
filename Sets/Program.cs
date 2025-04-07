8c8e53589058502c433a2bf99c3b9a9c73edad7b
MEGA <MEGA@gmail.com>
2025-04-07 18:46:02 +0300

    ex 1

--- Sets/Program.cs
+++ Sets/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Zad1();
+        }
+
+        static void Zad1()
+        {
+            Console.WriteLine("Ex 1");
+            Console.WriteLine("Enter numbers");
+            HashSet<int> list = new HashSet<int>();
+            list = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToHashSet();
+            Console.WriteLine($"Uniques are: {String.Join(',', list)}");
+        }
+        static void Zad2()
+        {
+
+        }
+        static void Zad3()
+        {
+
+        }
+        static void Zad4()
+        {
+
         }
     }
 }
